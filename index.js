a2c3726b7b6fc4fe62855719b890f75649784d96
crawforde <crawforde@kenyon.edu>
2017-11-16 11:21:49 -0800

    Set up postgres connection with pg

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var db = require('./pool.js');
 
 app.get('/',function(req,res){
   res.send('Server running!');
---
+++ pool.js
@@ -0,0 +1,23 @@
+"use strict";
+var pg = require('pg');
+
+if (!process.env.DATABASE_URL) {
+  console.error("DATABASE_URL environment variable missing.");
+  process.exit(1);
+}
+
+// Establish a connection to Postgres here using pg.Pool
+var pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL
+});
+
+pool.query('SELECT NOW()', (err, res) => {
+  if (err) {
+    console.log('Error running query', err);
+  } else {
+    console.log('Success, you are connected to Postgres');
+  }
+});
+
+// Export 'pool' so other files can use Postgres
+module.exports = pool;
